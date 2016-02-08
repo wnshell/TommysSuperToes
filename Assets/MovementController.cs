8aa9365211326aaff6f6c2e56ef0470ff0daa5d6
Ben Willshire <bwillsh@umich.edu>
2016-02-08 15:30:07 -0500

    Commented overworld code

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MovementController : MonoBehaviour {
 
     private Vector2 mousePos;
+
+    //Used to figure out the hit box of the buttons
+    //Created a square with size of 3x3 and covered up each of the buttons. the position of the square is the vectors below
     float squareSize = 1.5f;
     Vector2 buttonPos = new Vector2( -.14f, -3 );
     Vector2 leftPos = new Vector2(-4.37f, -3);
@@ -12,7 +15,9 @@ public class MovementController : MonoBehaviour {
 
 
     public GameObject background;
-    public float speed = 1;
+
+    //speed must be changed in the unity editor, at least for right now.
+    public float speed = 5;
 
     // Use this for initialization
     void Start () {
@@ -22,21 +27,23 @@ public class MovementController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        //copied from Foot.cs
         UpdateMousePos();
 
-        if(Input.GetMouseButtonDown(0))
+        
+        if(Input.GetMouseButtonDown(0)) //returns true if this is the first frame that the LMB was pressed, good for button presses
         {
             if(mousePos.x >= buttonPos.x - squareSize && mousePos.x <= buttonPos.x + squareSize
-                && mousePos.y >= buttonPos.y - squareSize && mousePos.y <= buttonPos.y + squareSize)
+                && mousePos.y >= buttonPos.y - squareSize && mousePos.y <= buttonPos.y + squareSize) //Math to check to see if the mouse is inside a hit box for a button
             {
                 ButtonPressed();
             }
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)) //returns true if LMB is pressed in this frame, good for button holds
         {
             if (mousePos.x >= leftPos.x - squareSize && mousePos.x <= leftPos.x + squareSize
-                && mousePos.y >= leftPos.y - squareSize && mousePos.y <= leftPos.y + squareSize)
+                && mousePos.y >= leftPos.y - squareSize && mousePos.y <= leftPos.y + squareSize)//More math
             {
                 GoLeft();
             }
@@ -56,11 +63,13 @@ public class MovementController : MonoBehaviour {
 
     void ButtonPressed()
     {
-
+        //Add Code Later
     }
 
     void GoLeft()
     {
+        //updates the background object's position
+        //the player character is static, so GoLeft() moves the background right
         Vector2 bpos = background.transform.position;
         bpos.x += speed * Time.deltaTime;
         background.transform.position = bpos;
