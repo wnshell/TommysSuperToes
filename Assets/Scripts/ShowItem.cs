4c81fd08a6d7275e7008db6d610c56b4528e75b8
Ryan Whitfield <ryan@0587387841.wireless.umich.net>
2016-02-17 16:13:22 -0500

    Add pencil dialogue bubble

---
+++ Assets/Scripts/ShowItem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowItem : MonoBehaviour {
+
+	public GameObject item;
+	public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		print ("triggered");
+		item.SetActive (true);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		item.SetActive (false);
+		text.SetActive (false);
+	}
+
+	void Button() {
+		text.SetActive (!text.active);
+		print ("I need an item!");
+	}
+}
